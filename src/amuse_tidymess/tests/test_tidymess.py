e7953558665d110d3f3d9cecf77d9d225eb03ba1
elkogerville <elkogerville@gmail.com>
2026-01-07 17:33:16 -0500

    test_tidymess import fixes

--- src/amuse_tidymess/tests/test_tidymess.py
+++ src/amuse_tidymess/tests/test_tidymess.py
@@ -5,7 +5,7 @@ from amuse.support.testing.amusetest import TestWithMPI
 from amuse_tidymess.interface import Tidymess, TidymessInterface
 from amuse.units import units as u
 from amuse.units import constants as c
-from amuse.units import generic_unit_system, nbody_system
+from amuse.units import nbody_system
 from amuse.datamodel import Particles
 from amuse.ext.orbital_elements import generate_binaries, new_binary_from_orbital_elements
 
