9c465b5d4242781c89d1c4edc69e7e5f63e2b3c3
HannoSpreeuw <h.spreeuw@esciencecenter.nl>
2026-03-04 11:09:28 +0100

    This code should not be needed.
    According to the AMUSE guideline https://amuse.readthedocs.io/en/latest/tutorial/legacy_code.html we should be able to leave "src/amuse_ph4/interface.cc" untouched, i.e. exactly the same as in the "main" branch.
    I.e., this code should not be necessary.

--- src/amuse_ph4/interface.cc
+++ src/amuse_ph4/interface.cc
@@ -935,14 +935,4 @@ int get_id_of_updated_particle(int index, int * index_of_particle, int * status)
     *index_of_particle = jd->UpdatedParticles[index].index_of_particle;
     *status = jd->UpdatedParticles[index].status;
     return 0;
-}
-
-extern "C" {
-    int sapporo_get_nj_max(int *value);
-}
-
-int get_nj_max(int *value)
-{
-    if (!jd) return -1;
-    return sapporo_get_nj_max(value);
-}
+ }
