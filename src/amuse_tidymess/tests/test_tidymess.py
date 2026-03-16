34bbdea5fdcf98d436faf31a2b2a762c7da24330
elkogerville <elkogerville@gmail.com>
2026-03-16 15:41:53 +0100

    added HD80606b initial conditions to tidymess tests

--- src/amuse_tidymess/tests/test_tidymess.py
+++ src/amuse_tidymess/tests/test_tidymess.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from amuse.support.testing.amusetest import TestWithMPI
+from amuse.units.quantities import VectorQuantity
 from amuse_tidymess.interface import Tidymess, TidymessInterface
 from amuse.units import units as u
 from amuse.units import constants as c
@@ -347,6 +348,53 @@ class TestTidymess(TestWithMPI):
         system.move_to_center()
 
         return system
+
+
+    def HD80606b_system(self):
+        """
+        Initial conditions for the exoplanet system
+        HD80606b. Initial conditions from Tidymess.
+        """
+        HD80606 = Particles(2)
+        star, planet = generate_binaries(
+            primary_mass=2.0088092e30 | u.kg,
+            secondary_mass=7.7459434e27 | u.kg,
+            semi_major_axis=0.455 | u.au,
+            eccentricity=0.9330
+        )
+        HD80606[0].name = 'HD80606'
+        HD80606[0].mass = star.mass
+        HD80606[0].radius = 702455.0 | u.km
+        HD80606[0].x = star.x.as_quantity_in(u.km)
+        HD80606[0].y = star.y.as_quantity_in(u.km)
+        HD80606[0].z = star.z.as_quantity_in(u.km)
+        HD80606[0].vx = star.vx.as_quantity_in(u.kms)
+        HD80606[0].vy = star.vy.as_quantity_in(u.kms)
+        HD80606[0].vz = star.vz.as_quantity_in(u.kms)
+        HD80606[0].xi = 0.07
+        HD80606[0].kf = 0.0
+        HD80606[0].tau = 0.0 | u.yr
+        HD80606[0].wx = 0.0 | 1 / u.s
+        HD80606[0].wy = 0.0 | 1 / u.s
+        HD80606[0].wz = 2.97188607137e-6 | 1 / u.s
+
+        HD80606[1].name = 'HD80606b'
+        HD80606[1].mass = planet.mass
+        HD80606[1].radius = 68488.3446 | u.km
+        HD80606[1].x = planet.x.as_quantity_in(u.km)
+        HD80606[1].y = planet.y.as_quantity_in(u.km)
+        HD80606[1].z = planet.z.as_quantity_in(u.km)
+        HD80606[1].vx = planet.vx.as_quantity_in(u.kms)
+        HD80606[1].vy = planet.vy.as_quantity_in(u.kms)
+        HD80606[1].vz = planet.vz.as_quantity_in(u.kms)
+        HD80606[1].xi = 0.25
+        HD80606[1].kf = 0.5
+        HD80606[1].tau = 1e-2 | u.yr
+        HD80606[1].wx = 0.0 | 1 / u.s
+        HD80606[1].wy = 0.0 | 1 / u.s
+        HD80606[1].wz = 0.000145444104333 | 1 / u.s
+
+        return HD80606
 
     def test1(self):
         """
