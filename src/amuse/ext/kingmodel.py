8bf1687d8112d53b4a1e8858730560deb7d9da4c
elkogerville <elkogerville@gmail.com>
2026-04-07 11:42:34 +0200

    added deprication warning to amuse.ext.kingmodel

--- src/amuse/ext/kingmodel.py
+++ src/amuse/ext/kingmodel.py
@@ -1,1 +1,13 @@
-from amuse.ic.kingmodel import *
+import warnings
+from amuse.ic.kingmodel import (
+    MakeKingModel,
+    new_king_model
+)
+
+warnings.warn(
+    'amuse.ext.kingmodel is deprecated and will be removed in the future. '
+    'Import from amuse.ic.kingmodel instead: '
+    '"from amuse.ic.kingmodel import ..."',
+    category=DeprecationWarning,
+    stacklevel=2
+)
