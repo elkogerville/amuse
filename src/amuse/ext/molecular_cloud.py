394d768f1b4c5e74f747afc752e997e25014e1b2
elkogerville <elkogerville@gmail.com>
2026-04-07 11:51:04 +0200

    cleaned up molecular_cloud.py in ext

--- src/amuse/ext/molecular_cloud.py
+++ src/amuse/ext/molecular_cloud.py
@@ -1,5 +1,14 @@
 import warnings
-from amuse.ic.molecular_cloud import *
+from amuse.ic.molecular_cloud import (
+    constant_density_div_free_power_law_v_ism_cube,
+    molecular_cloud,
+    interpolate_trilinear,
+    ism_cube,
+    make_div_free,
+    make_ifft_real,
+    new_ism_cube,
+    random_field
+)
 
 warnings.warn(
     'amuse.ext.molecular_cloud is deprecated and will be removed in the future. '
