472c2a7333047317181020f777e5277d1c70986c
elkogerville <elkogerville@gmail.com>
2026-04-07 14:44:52 +0200

    cleaned up plummer.py

--- src/amuse/ext/plummer.py
+++ src/amuse/ext/plummer.py
@@ -1,5 +1,14 @@
 import warnings
 
-from amuse.ic.plummer import *
+from amuse.ic.plummer import (
+    MakePlummerModel,
+    new_plummer_model,
+    new_plummer_sphere
+)
 
-warnings.warn("amuse.ext.plummer has moved to amuse.ic.plummer", DeprecationWarning)
+warnings.warn(
+    'amuse.ext.plummer is deprecated and will be removed in the future. '
+    'Import from amuse.ic.plummer instead: "from amuse.ic.plummer import ..."',
+    category=DeprecationWarning,
+    stacklevel=2
+)
