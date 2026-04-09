c73c86bb9aa2db4c76d116737340f30b460805e4
elkogerville <elkogerville@gmail.com>
2026-04-09 11:41:31 +0200

    fixed salpeter.py deprecation warning in ext

--- src/amuse/ext/salpeter.py
+++ src/amuse/ext/salpeter.py
@@ -1,5 +1,13 @@
 import warnings
 
-from amuse.ic.salpeter import *
+from amuse.ic.salpeter import (
+    new_salpeter_mass_distribution,
+    new_salpeter_mass_distribution_nbody
+)
 
-warnings.warn("amuse.ext.salpeter has moved to amuse.ic.salpeter", DeprecationWarning)
+warnings.warn(
+    'amuse.ext.salpeter is deprecated and will be removed in the future. '
+    'Import from amuse.ic.salpeter instead: "from amuse.ic.salpeter import ..."',
+    category=DeprecationWarning,
+    stacklevel=2
+)
