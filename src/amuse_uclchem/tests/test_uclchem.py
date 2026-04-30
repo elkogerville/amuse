490b10d7c2cb2d0f194656d8ed8183394a27242c
elkogerville <elkogerville@gmail.com>
2026-04-30 16:51:22 +0200

    uclchem tests

--- src/amuse_uclchem/tests/test_uclchem.py
+++ src/amuse_uclchem/tests/test_uclchem.py
@@ -11,6 +11,8 @@ class TestUclchemInterface(TestWithMPI):
         assert instance is not None
 
         instance.new_particle(1, 2, 3, 4)
+        instance.new_particle(11, 12, 13, 14)
+
         result = instance.get_state(0)
         self.assertEquals(result['number_density'], 1)
         self.assertEquals(result['temperature'], 2)
@@ -41,6 +43,20 @@ class TestUclchem(TestWithMPI):
 
         return p
 
+    def generate_chem_particles(self):
+        p = Particles(2)
+        p[0].number_density = 1 | u.cm**-3
+        p[0].temperature = 2 | u.K
+        p[0].ionrate = 3 | u.s**-1
+        p[0].radfield = 4 | habing
+
+        p[1].number_density = 5 | u.cm**-3
+        p[1].temperature = 6 | u.K
+        p[1].ionrate = 7 | u.s**-1
+        p[1].radfield = 8 | habing
+
+        return p
+
     def test_add_particle(self):
 
         p = self.generate_chem_particle()
@@ -49,5 +65,16 @@ class TestUclchem(TestWithMPI):
 
         instance.commit_parameters()
         instance.particles.add_particle(p)
+
+        instance.stop()
+
+    def test_add_particles(self):
+
+        p = self.generate_chem_particles()
+        instance = self.new_instance_of_an_optional_code(Uclchem, redirection='none')
+        assert instance is not None
+
+        instance.commit_parameters()
+        instance.particles.add_particles(p)
 
         instance.stop()
