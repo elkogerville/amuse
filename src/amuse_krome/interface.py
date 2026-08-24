b15b72e1138a427247c00e3e894fd6ca69c85c1a
elkogerville <elkogerville@gmail.com>
2026-08-24 16:16:48 +0200

    make Krome depend on ChemicalEvolution

--- src/amuse_krome/interface.py
+++ src/amuse_krome/interface.py
@@ -86,56 +86,30 @@ class KromeInterface(
         function.result_type = 'i'
         return function
 
-    @legacy_function
-    def evolve_model():
-        function = LegacyFunctionSpecification()
-        function.addParameter('time_end', dtype='d', direction=function.IN)
-        function.result_type = 'i'
-        return function
 
-    @legacy_function
-    def get_time():
-        function = LegacyFunctionSpecification()
-        function.addParameter('time', dtype='float64', direction=function.OUT)
-        function.result_type = 'int32'
-        return function
-
-    @legacy_function   
-    def delete_particle():
-        function = LegacyFunctionSpecification()
-        function.can_handle_array = True
-        function.addParameter('id', dtype='int32', direction=function.IN)
-        function.result_type = 'i'
-        return function
-
-class Krome(CommonCode):
+class Krome(ChemicalEvolution):
 
     def __init__(self,unit_converter = None, **options):
-        
+
         if unit_converter is not None:
-            raise Exception("krome uses predefined units")
-           
-        InCodeComponentImplementation.__init__(self, KromeInterface(**options))
+            raise Exception('krome uses predefined units')
+
+        ChemicalEvolution.__init__(self, KromeInterface(**options))
+
         first,last=self.get_firstlast_abundance()
         self.species=dict()
         for i in range(first,last+1):
           self.species[self.get_name_of_species(i)]=i-1
-#        self.set_data_directory(self.data_directory())
-#        self.set_output_directory(self.output_directory())
-                  
+
     def define_properties(self, handler):
         handler.add_property('get_time', public_name = "model_time")
 
     def define_methods(self, handler):
-        CommonCode.define_methods(self, handler)
+        ChemicalEvolution.define_methods(self, handler)
         handler.add_method(
-            'evolve_model', 
-            (
-                units.s,
-            ), 
-            (
-                handler.ERROR_CODE,
-            )
+            'evolve_model',
+            (units.s,),
+            (handler.ERROR_CODE,)
         )
         handler.add_method(
             "new_particle",
@@ -232,22 +206,3 @@ class Krome(CommonCode):
         handler.add_getter('particles', 'get_state')
         handler.add_gridded_getter('particles', 'get_abundance','get_firstlast_abundance', names = ('abundances',))
         handler.add_gridded_setter('particles', 'set_abundance','get_firstlast_abundance', names = ('abundances',))
-
-    def define_state(self, handler):
-        CommonCode.define_state(self, handler)
-        handler.add_transition('INITIALIZED','EDIT','commit_parameters')
-        handler.add_transition('RUN','PARAMETER_CHANGE_A','invoke_state_change2')
-        handler.add_transition('EDIT','PARAMETER_CHANGE_B','invoke_state_change2')
-        handler.add_transition('PARAMETER_CHANGE_A','RUN','recommit_parameters')
-        handler.add_transition('PARAMETER_CHANGE_B','EDIT','recommit_parameters')
-        handler.add_method('EDIT', 'new_particle')
-        handler.add_method('EDIT', 'delete_particle')
-        handler.add_transition('EDIT', 'RUN', 'commit_particles')
-        handler.add_transition('RUN', 'UPDATE', 'new_particle', False)
-        handler.add_transition('RUN', 'UPDATE', 'delete_particle', False)
-        handler.add_transition('UPDATE', 'RUN', 'recommit_particles')
-        handler.add_method('RUN', 'evolve_model')
-        handler.add_method('RUN', 'get_state')
-        handler.add_method('RUN', 'get_abundance')
-
-
