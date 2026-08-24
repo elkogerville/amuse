a11da7deb561c081631e66be24eca48f12f78cf6
elkogerville <elkogerville@gmail.com>
2026-08-24 06:18:43 +0200

    class ChemicalEvolution

--- src/amuse/community/interface/chem.py
+++ src/amuse/community/interface/chem.py
@@ -110,3 +110,79 @@ class ChemicalEvolutionInterface(common.CommonCodeInterface):
                 Unable to determine the count
         """
         return function
+
+class ChemicalEvolution(common.CommonCode):
+
+    def __init__(self, legacy_interface, unit_converter=None, **options):
+        self.unit_converter = unit_converter
+
+        common.CommonCode.__init__(self, legacy_interface, **options)
+
+    def define_properties(self, handler):
+        handler.add_property('get_time', public_name='model_time')
+
+    def define_methods(self, handler):
+        common.CommonCode.define_methods(self, handler)
+
+        handler.add_method('evolve_model', (u.yr,), (handler.ERROR_CODE,))
+
+        handler.add_method(
+            'delete_particle', (handler.INDEX,), (handler.ERROR_CODE,)
+        )
+
+        handler.add_method(
+            'get_abundance',
+            (handler.INDEX, handler.INDEX,),
+            (handler.NO_UNIT, handler.ERROR_CODE,),
+        )
+
+        handler.add_method(
+            'set_abundance',
+            (
+                handler.INDEX,
+                handler.INDEX,
+                handler.NO_UNIT,
+            ),
+            (handler.ERROR_CODE,),
+        )
+
+        handler.add_method(
+            'get_firstlast_abundance',
+            (),
+            (
+                handler.NO_UNIT,
+                handler.NO_UNIT,
+                handler.ERROR_CODE,
+            )
+        )
+
+        handler.add_method(
+            'get_time',
+            (),
+            (u.yr, handler.ERROR_CODE,),
+        )
+
+        handler.add_method(
+            'get_number_of_particles',
+            (),
+            (handler.NO_UNIT, handler.ERROR_CODE,),
+        )
+
+    def define_particle_sets(self, handler):
+        handler.define_set('particles', 'index_of_the_particle')
+        handler.set_new('particles', 'new_particle')
+        handler.set_delete('particles', 'delete_particle')
+        handler.add_setter('particles', 'set_state')
+        handler.add_getter('particles', 'get_state')
+        handler.add_gridded_getter(
+            'particles',
+            'get_abundance',
+            'get_firstlast_abundance',
+            names=('abundances',),
+        )
+        handler.add_gridded_setter(
+            'particles',
+            'set_abundance',
+            'get_firstlast_abundance',
+            names=('abundances',),
+        )
