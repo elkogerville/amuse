d183dc5203033cb51605093fd71ffb9325526351
elkogerville <elkogerville@gmail.com>
2026-08-27 15:54:40 +0200

    added more particleset definitions to krome

--- src/amuse_krome/interface.py
+++ src/amuse_krome/interface.py
@@ -177,3 +177,10 @@ class Krome(ChemicalEvolution):
             (),
             (units.s, handler.ERROR_CODE,)
         )
+
+        def define_particle_sets(self, handler):
+            ChemicalEvolution.define_particle_sets(self, handler)
+            handler.add_getter('particles', 'get_density')
+            handler.add_setter('particles', 'set_density')
+            handler.add_getter('particles', 'get_internal_energy')
+            handler.add_setter('particles', 'set_internal_energy')
