f52b5095eb819a671511c3ed953f87964c2eafb3
elkogerville <elkogerville@gmail.com>
2026-09-14 16:37:52 +0200

    modernized krome interface.f90

--- src/amuse_krome/interface.f90
+++ src/amuse_krome/interface.f90
@@ -1,126 +1,165 @@
 function initialize_code() result(ret)
-  use chem_mod
+  use chem_mod, only : chem_initialize
+  implicit none
   integer :: ret
-  ret=chem_initialize()
-end function
+  ret = chem_initialize()
+end function initialize_code
 
 function cleanup_code() result(ret)
-  use chem_mod
+  use chem_mod, only : chem_end
+  implicit none
   integer :: ret
-  ret=chem_end()
-end function
-
-function commit_particles() result(ret)
-  use chem_mod
-  integer :: ret
-  ret=chem_commit_particles()
-end function
-
-function recommit_particles() result(ret)
-  use chem_mod
-  integer :: ret
-  ret=chem_commit_particles()
-end function
+  ret = chem_end()
+end function cleanup_code
 
 function commit_parameters() result(ret)
-  use chem_mod
+  use chem_mod, only : chem_commit_parameters
+  implicit none
   integer :: ret
-  ret=chem_commit_parameters()
-end function
+  ret = chem_commit_parameters()
+end function commit_parameters
 
 function recommit_parameters() result(ret)
-  use chem_mod
+  use chem_mod, only : chem_commit_parameters
+  implicit none
   integer :: ret
-  ret=chem_commit_parameters()
-end function
+  ret = chem_commit_parameters()
+end function recommit_parameters
 
-function get_number_of_particles(n) result(ret)
-  use chem_mod
-  integer n,ret
-  n=nparticle
-  ret=0
-end function
+function commit_particles() result(ret)
+  use chem_mod, only : chem_commit_particles
+  implicit none
+  integer :: ret
+  ret = chem_commit_particles()
+end function commit_particles
 
-function new_particle(id,density,temperature,ionrate) result(ret)
-  use chem_mod
-  integer :: ret,id
-  double precision :: density,temperature,ionrate 
-  ret=add_particle(id,density,temperature,ionrate)
-end function
+function recommit_particles() result(ret)
+  use chem_mod, only : chem_commit_particles
+  implicit none
+  integer :: ret
+  ret = chem_commit_particles()
+end function recommit_particles
 
-function set_state(id,density,temperature,ionrate) result(ret)   
-  use chem_mod
-  integer :: ret,id
-  double precision :: density,temperature,ionrate
-  ret=set_particle_state(id,density,temperature,ionrate)
-end function
+function new_particle(index_of_the_particle, number_density, temperature, ionrate) result(ret)
+  use chem_mod, only : add_particle
+  implicit none
+  integer, intent(out) :: index_of_the_particle
+  double precision, intent(in) :: number_density, temperature, ionrate
+  integer :: ret
+  ret = add_particle(index_of_the_particle, number_density, temperature, ionrate)
+end function new_particle
 
-function get_state(id,density,temperature,ionrate) result(ret)   
-  use chem_mod
-  integer :: ret,id
-  double precision :: density,temperature,ionrate
-  ret=get_particle_state(id,density,temperature,ionrate)
-end function
+function delete_particle(index_of_the_particle) result(ret)
+  use chem_mod, only : remove_particle
+  implicit none
+  integer, intent(in) :: index_of_the_particle
+  integer :: ret
+  ret = remove_particle(index_of_the_particle)
+end function delete_particle
 
-function get_abundance(id,aid,x) result(ret)
-  use chem_mod
-  integer ret,id,aid
-  double precision x
-  ret=get_particle_abundance(id,aid,x)
-end function
+function get_state(index_of_the_particle, number_density, temperature, ionrate) result(ret)
+  use chem_mod, only : get_particle_state
+  implicit none
+  integer, intent(in) :: index_of_the_particle
+  double precision, intent(out) :: number_density, temperature, ionrate
+  integer :: ret
+  ret = get_particle_state(index_of_the_particle, number_density, temperature, ionrate)
+end function get_state
 
-function set_abundance(id,aid,x) result(ret)
-  use chem_mod
-  integer ret,id,aid
-  double precision x
-  ret=set_particle_abundance(id,aid,x)
-end function
+function set_state(index_of_the_particle, number_density, temperature, ionrate) result(ret)
+  use chem_mod, only : set_particle_state
+  implicit none
+  integer, intent(in) :: index_of_the_particle
+  double precision, intent(in) :: number_density, temperature, ionrate
+  integer :: ret
+  ret = set_particle_state(index_of_the_particle, number_density, temperature, ionrate)
+end function set_state
 
-function get_firstlast_abundance(first,last) result(ret)
-  use chem_mod
-  integer ret,first,last
-  first=1
-  last=krome_nmols ! this is the last species defined in amuse_helpers.f90
-  ret=0
-end function
+function get_abundance(index_of_the_particle, species_index, abundance) result(ret)
+  use chem_mod, only : get_particle_abundance
+  implicit none
+  integer, intent(in) :: index_of_the_particle, species_index
+  double precision, intent(out) :: abundance
+  integer :: ret
+  ret = get_particle_abundance(index_of_the_particle, species_index, abundance)
+end function get_abundance
 
-function get_name_of_species(i,s) result(ret)
-  use chem_mod
-  integer ret,i
-  character*16 :: ss(krome_nmols),s
-  if(i.LT.1.OR.i.GT.krome_nmols) then
-    ret=-1
+function set_abundance(index_of_the_particle, species_index, abundance) result(ret)
+  use chem_mod, only : set_particle_abundance
+  implicit none
+  integer, intent(in) :: index_of_the_particle, species_index
+  double precision, intent(in) :: abundance
+  integer :: ret
+  ret = set_particle_abundance(index_of_the_particle, species_index, abundance)
+end function set_abundance
+
+function set_abundances(index_of_the_particle, abundances, N) result(ret)
+  use chem_mod, only : set_particle_abundances
+  implicit none
+  integer, intent(in) :: N
+  integer, intent(in) :: index_of_the_particle(N)
+  double precision, intent(in) :: abundances(N)
+  integer :: ret
+  ret = set_particle_abundances(index_of_the_particle, abundances, N)
+end function set_abundances
+
+function get_firstlast_species_index(first, last) result(ret)
+  use chem_mod, only : krome_nmols
+  implicit none
+  integer, intent(out) :: first, last
+  integer :: ret
+  first = 1
+  last = krome_nmols ! this is the last species defined in amuse_helpers.f90
+  ret = 0
+end function get_firstlast_species_index
+
+function get_species_index(name, species_index) result(ret)
+  use chem_mod, only : krome_get_index
+  implicit none
+  character(len=16), intent(in) :: name
+  integer, intent(out) :: species_index
+  integer :: ret
+  species_index = krome_get_index(name)
+  ret = 0
+end function get_species_index
+
+function get_species_name(species_index, name) result(ret)
+  use chem_mod, only : krome_get_names, krome_nmols
+  implicit none
+  integer, intent(in) :: species_index
+  character(len=16), intent(out) :: name
+  integer :: ret
+  character(len=16) :: names(krome_nmols)
+  if(species_index .LT. 1 .OR. species_index .GT. krome_nmols) then
+    ret = -1
     return
   endif
-  ss=krome_get_names()
-  s=ss(i)
-  ret=0
-end function
+  names = krome_get_names()
+  name = names(species_index)
+  ret = 0
+end function get_species_name
 
-function get_index_of_species(s,i) result(ret)
-  use chem_mod
-  integer ret,i
-  character*16 s
-  i=krome_get_index(s)
-  ret=0
-end function
-
-function evolve_model(tend) result(ret)
-  use chem_mod
+function evolve_model(time) result(ret)
+  use chem_mod, only : evolve_chem_model
+  implicit none
+  double precision, intent(in) :: time
   integer :: ret
-  double precision :: tend  
-  ret=evolve_chem_model(tend)
-end function
+  ret = evolve_chem_model(time)
+end function evolve_model
 
-function get_time(outtime) result(ret)
-  use chem_mod
+function get_time(time) result(ret)
+  use chem_mod, only : chem_model_time
+  implicit none
+  double precision, intent(out) :: time
   integer :: ret
-  double precision :: outtime  
-  ret=chem_model_time(outtime)
-end function
+  ret = chem_model_time(time)
+end function get_time
 
-function delete_particle(id) result(ret)
-  use chem_mod
-  integer :: ret,id
-  ret=remove_particle(id)
-end function
+function get_number_of_particles(number_of_particles) result(ret)
+  use chem_mod, only : nparticle
+  implicit none
+  integer, intent(out) :: number_of_particles
+  integer :: ret
+  number_of_particles = nparticle
+  ret = 0
+end function get_number_of_particles
