4191292233a7b74a865f2796e677bcd5b2d91762
elkogerville <elkogerville@gmail.com>
2026-09-16 16:03:29 +0200

    krome_constants_amuse.f90 kromesph

---
+++ src/amuse_kromesph/src/krome_constants_amuse.f90
@@ -0,0 +1,8 @@
+! This module exists so that AMUSE can send the
+! atomic mass unit value in grams to Krome at runtime.
+module krome_constants_amuse
+  implicit none
+
+  double precision :: amu_2_g
+
+end module krome_constants_amuse
